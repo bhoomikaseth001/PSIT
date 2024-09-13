79f2f7aadbdac92a433b61bad11d850724c6118a
Bhoomika <bhoomikaseth2004.com>
2024-09-13 15:47:48 +0530

    Fri, Sep 13, 2024  3:47:47 PM

--- SEMESTER 3/Python/LECTURE4.py
+++ SEMESTER 3/Python/LECTURE4.py
@@ -1,6 +1,13 @@
-'''WAP to take year of birth as input and then calculate the age and print it to the terminal'''
-'''WAP to enter the weight in pounds and convert into kg'''
-weight_pound=input("Enter your weight in pounds: ")
+# '''WAP to take year of birth as input and then calculate the age and print it to the terminal'''
+# '''WAP to enter the weight in pounds and convert into kg'''
+# weight_pound=input("Enter your weight in pounds: ")
 
-weight_kg=int(weight_pound)*0.453592
-print(weight_kg)
+# weight_kg=int(weight_pound)*0.453592
+# print(weight_kg)
+
+# '''WAP to print in multiline'''
+# print("Hi","I am","Bhoomika",sep="\n")
+
+x=10
+x=y+=2
+print(x) 
