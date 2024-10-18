11ebdd4669a14eff7cbb2e9bff253b4e5e37fc39
BHOOMIKA <bhoomikaseth999@gmail.com>
2024-10-18 16:10:42 +0530

    Fri, Oct 18, 2024  4:10:41 PM

--- SEMESTER 3/CodingBasics/array.cpp
+++ SEMESTER 3/CodingBasics/array.cpp
@@ -4,15 +4,17 @@
 //     int arr[]={2,5,67,3,-23,546,0};
 //     int smallest=INT_MAX;
 //     int largest=INT_MIN;
-//     int ind_larg,ind_smal;
+//     int small_ind=-1;
+//     int large_ind=-1;
 //     for(int i=0;i<sizeof(arr)/sizeof(int);i++){
-//         largest=max(arr[i],largest);
-//         ind_larg=i;
-//         smallest=min(arr[i],smallest);
-//         ind_smal=i;}
-//         cout<<"Largest value in the array is : "<<largest<<" at position "<<ind_larg<<endl;
-//         cout<<"Smallest value in the array is : "<<smallest<<" at position "<<ind_smal<<endl;
-
+//         if(arr[i]==(largest=max(arr[i],largest)))
+//             large_ind=i;
+//         if(arr[i]==(smallest=min(arr[i],smallest)))
+//              small_ind=i;
+//         }
+//         cout<<"Largest value in the array is : "<<largest<<" at position "<<large_ind<<endl;
+//         cout<<"Smallest value in the array is : "<<smallest<<" at position "<<small_ind<<endl;
+//   return 0;
 // }
 
 //REVERSE OF AN ARRAY
@@ -44,29 +46,28 @@
 
 /*What is an array?? - is a linear ds, has collection of elements of same data type, stored in contiguous memory locations 
 Why do require an array??  - to store multiple values in a single variable
-
 */
 //check if the number is the power of 2 or not
-#include<iostream>
-#include <cmath>
-using namespace std;
+// #include<iostream>
+// #include <cmath>
+// using namespace std;
 
-int main(){
-  int n;
-  cout<<"Enter a number: ";
-  cin>>n;
+// int main(){
+//   int n;
+//   cout<<"Enter a number: ";
+//   cin>>n;
   // double intpart;
   // double isPower=log2(n);
   // double fracpart=modf(isPower,&intpart);
   // cout<<"Integral part "<<intpart<<endl;
   // cout<<"Fractional part "<<fracpart<<endl;
 
-  // if(fracpart==0.0)
-  if(n & -n == n)
-    cout<<n<<" is a power of 2";
-  else
-    cout<<n<<" is not a power of 2";
-}
+  //if(fracpart==0.0)
+//   if((n & -n) == n)
+//     cout<<n<<" is a power of 2";
+//   else
+//     cout<<n<<" is not a power of 2";
+// }
 
 //REVERSE THE ARRAY
 /*   6          2         3          4         5  
@@ -101,12 +102,15 @@ int main(){
 // }
 
 
-//Unique elements
+/*UNIQUE ELEMENTS*/
 // #include <iostream>
 // #include <algorithm>
+// #include<set>
+// #include<unordered_set>
+// #include<vector>
 // using namespace std;
 // int main(){
-//   int arr[13]={1,2,1,3,2,6,3,1,3,2,4,3,1};
+   //int arr[13]={1,2,1,3,2,6,3,1,3,2,4,3,1};
 //   sort(arr,arr+13);
 //   for(int i=0;i<13;i++){
 //       if(arr[i]!=arr[i+1]){
@@ -115,6 +119,157 @@ int main(){
 //          }
 //     }
   
+// int arr[13]={1,2,1,3,2,6,3,1,3,2,4,3,1};
+//   unordered_set<int>st(arr, arr+13);
+//   for(int i:st)
+//     cout<<i<<" ";
+
+
 //   return 0;
 //   }
 
+
+/*SIMILAR QUESTION AS UNIQUE ELEMENT*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int arr[7]={2,3,1,6,3,6,2};
+//   int uniq=0;
+//   for(int i=0;i<7;i++)
+//     uniq^=arr[i];
+//   cout<<uniq;
+//   return 0;
+// }
+
+/*Swap atlernate elements*/
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() { 
+// int arr[5]={1,3,2,7,11};
+// int temp;
+// for(int i=0;i<5;i+=2){
+//   if (i+1<5)
+//     // swap(arr[i],arr[i+1]);
+//    temp=arr[i];
+//     arr[i]=arr[i+1];
+//     arr[i+1]=temp;
+// }
+//   for(int i=0;i<5;i++)
+//     cout<<arr[i]<<" ";
+
+// return 0;
+// }
+
+/*FIND DUPLICATES*/
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main (){
+  // int arr[8]={3,5,4,7,6,2,3,1};
+  // sort(arr,arr+8);
+  // for(int i=0;i<7;i++){
+  //     if(arr[i]==arr[i+1])
+  //       cout<<"Duplicate element is "<<arr[i];
+  // }
+
+  // vector<int>vec={3,5,4,7,6,2,3,1};
+  // unordered_map<int,int >mp;
+  // for(int &v:vec)
+  //   mp[v]++;
+  // for(auto &i:mp){
+  //   if (i.second==2)
+  //     cout<<"Duplicate element is : "<<i.first;
+  // }
+
+
+//    vector<int>vec={5,4,3,7,6,2,3,1};
+//    int dup=0;
+//    for(int i=0;i<vec.size();i++)
+//       dup^=vec[i];
+//   for(int i=1;i<vec.size();i++)
+//       dup^=i;
+//   cout<<dup;
+// return 0;
+    
+// }
+
+
+
+/*INTERSECTION OF TWO ARRAYS*/
+
+//BRUTE FORCE APP.
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int a1[7]={1,2,2,3,3,3,4};
+//   int a2[3]={2,2,3};
+//   vector<int>ans;
+//   for(int i=0;i<7;i++){
+//     int ele=a1[i];
+//     for(int j=0;j<3;j++){
+//       if(ele==a2[j]){
+//         ans.push_back(ele);
+//         a2[j]=INT_MIN; //IMPORTANT POINT
+//         break;
+//     }
+//     }
+//   }
+//   for(auto i:ans)
+//      cout<<i<<" ";
+// return 0;
+// }
+
+//OPTIMIZED APPROACH
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//   int a1[7]={1,2,2,3,3,3,4};
+//   int a2[3]={2,2,3};
+//   int i=0,j=0;
+//   vector<int>ans;
+//   while(i<7 && j<3){
+//     if(a1[i] == a2[j]){
+//       ans.push_back(a1[i]);
+//       i++;
+//       j++;
+//     }
+//     else if(a1[i]<a2[j])
+//       i++;
+//     else
+//       j++;
+//   }
+//   for(auto i:ans)
+//      cout<<i<<" ";
+// return 0;
+// }
+
+/*PAIR SUM*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  vector<int>nums={5,7,7,8,8,8,10};
+  int target=6;
+        int count;
+        vector<int>vec;
+       
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                vec.push_back(i);
+                
+                count=i;
+                for(int j=i;nums[j]==target;j++){
+                    count=j;
+                }
+                break;
+            }
+        }
+        vec.push_back(count);
+        
+        if(vec.empty())
+        cout<<"-1,-1";
+        else
+        {
+          for(int i=0;i<vec.size();i++)
+        cout<<vec[i]<<" ";
+        }
+    }
